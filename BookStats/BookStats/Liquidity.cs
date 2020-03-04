8690896c24ccc8503117617d744432cf035e2ffa
Harry Dennen <Harry.Dennen@derivco.co.za>
2020-03-04 10:14:23 +0200

    add basic api call

---
+++ BookStats/BookStats/Liquidity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace BookStats
+{
+    class Liquidity
+    {
+        static HttpClient client = new HttpClient();
+        static String url = "https://api-pub.bitfinex.com/v2/";
+        static String pathParams = "book/tBTCUSD/P0"; // Change based on desired pairs and precision
+        static String queryParams = ""; // Change these based on relevant query params
+
+        public static async Task GetBookSnapshot() 
+        {
+            String bookEntries;
+            HttpResponseMessage response = await client.GetAsync(url + pathParams);
+
+            if (response.IsSuccessStatusCode)
+            {
+                bookEntries = await response.Content.ReadAsStringAsync();
+                String entriesJson = "{\"entries\": " + bookEntries + "}";
+                JObject jsonEntries = JObject.Parse(entriesJson);
+
+                Console.WriteLine(jsonEntries.First);
+                //Console.WriteLine(bookEntries);
+            }
+        }
+    }
+}
---
+++ BookStats/BookStats/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BookStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Liquidity.GetBookSnapshot().GetAwaiter().GetResult();
+
+        }
+    }
+}
